1809e5b3e8e82c74f7d7e55b5f93e28f8927cb78
Vitalii <mohara88s@gmail.com>
2026-03-05 09:07:23 +0000

    make task 1

---
+++ create_topics.py
@@ -0,0 +1,34 @@
+from kafka.admin import KafkaAdminClient, NewTopic
+from configs import kafka_config
+
+# Створення клієнта Kafka
+admin_client = KafkaAdminClient(
+    bootstrap_servers=kafka_config['bootstrap_servers'],
+    security_protocol=kafka_config['security_protocol'],
+    sasl_mechanism=kafka_config['sasl_mechanism'],
+    sasl_plain_username=kafka_config['username'],
+    sasl_plain_password=kafka_config['password']
+)
+
+# Визначення нових топіків
+my_name = "Vitalii_vasylets"
+topic_names = [f'{my_name}_building_sensors',f'{my_name}_temperature_alerts',f'{my_name}_humidity_alerts']
+num_partitions = 2
+replication_factor = 1
+
+new_topics = [
+    NewTopic(name=topic_name, num_partitions=num_partitions, replication_factor=replication_factor) for topic_name in topic_names
+]
+
+# Створення нових топіків
+try:
+    admin_client.create_topics(new_topics=new_topics, validate_only=False)
+    print(f"Topics '{topic_names}' created successfully.")
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+# Перевіряємо список існуючих топіків 
+print(admin_client.list_topics())
+
+# Закриття зв'язку з клієнтом
+admin_client.close()
